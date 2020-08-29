c47fa7c0726849186fe5126fd7adcee0a6f1c941
Erik Alfvin <erikalfvin@gmail.com>
2020-08-29 17:27:03 -0500

    Changed background and blue colors

--- Configs.py
+++ Configs.py
@@ -1,5 +1,5 @@
 class Configs():
     #Colors
-    menuColor = (0.155, 0.217, 0.292, 0.65)
+    menuColor = (0.11, 0.17, 0.242, 0.7)
     backgroundColor = (0.936, 0.938, 0.946, 1)
     inputButtonColor = (0.13, 0.5, 0.95, 0.94)
