5643cdda20888b7bcea01630e72a72ab826bb7f2
Erik Alfvin <erikalfvin@gmail.com>
2020-09-11 21:23:00 -0500

    Write out calculated masses

--- WriteOutFile.py
+++ WriteOutFile.py
@@ -192,8 +192,8 @@ def writeMasses(series, f):
 
         line.append(series.weightDensities[i])
         line.append(series.weightCCEs[i])
-        line.append(float(series.matrixBHat[i][0]))
-        line.append(float(series.matrixBHat[i][0] - series.weightNominals[0][i]) * 1000)
+        line.append(float(series.calculatedMasses[0][i]))
+        line.append(float(series.calculatedMasses[0][i] - series.weightNominals[0][i]) * 1000)
         
         table.append(line)
 
