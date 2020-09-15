cbeff4a06854029c00e3c261c12d67f222344b78
Erik Alfvin <erikalfvin@gmail.com>
2020-09-14 21:16:25 -0500

    Tweak menu width and color

--- Configs.py
+++ Configs.py
@@ -1,6 +1,6 @@
 class Configs():
     #Colors
-    menuColor = (0.11, 0.17, 0.242, 0.7)
+    menuColor = (0.105, 0.165, 0.23, 0.71)
     backgroundColor = (0.936, 0.938, 0.946, 1)
     inputButtonColor = (0.13, 0.5, 0.95, 0.94)
     greenButtonColor = (0.0314, 0.62, 0.165, 0.9)
